721b498d127e0083fd23dcb6e070eef0edacecd1
Nitins <nitins2332@gmail.com>
2022-02-17 13:40:29 +0530

    change templete to start react again

--- src/App.js
+++ src/App.js
@@ -1,68 +1,10 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import Recipes from "./Recipe";
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import React from "react";
 
 function App() {
-  const APP_ID = "e67c5687";
-  const APP_KEY = "e0f5c419f875bb8370d8ef7207b0a360";
-  // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  // const [counter, setCounter] = useState(0);
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
-
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
-
-  const getRecipes = async () => {
-    const responce = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    const data = await responce.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  };
-
-  const updateSearch = (event) => {
-    setSearch(event.target.value);
-    // console.log(search);
-  };
-
-  const getSearch = (event) => {
-    event.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
-
   return (
     <div className="app">
-      <form onSubmit={getSearch} className="search-form">
-        <input
-          type="text"
-          className="search-bar"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-        {recipes.map((recipe) => (
-          <Recipes
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-          />
-        ))}
-      </div>
-      {/* <Recipes /> */}
+      <h2>learn react router</h2>
     </div>
   );
 }
